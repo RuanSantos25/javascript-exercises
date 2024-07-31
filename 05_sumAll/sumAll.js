b9bfd9d2a5266854e8458aa2df59e874f82c4c0b
RuanSantos25 <[email protected]>
2024-07-30 21:37:56 -0300

    Add answers to 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    numbers = args.sort();
+    firstNumber = numbers[0];
+    lastNumber = numbers[1];  
+    if (firstNumber < 0 || lastNumber < 0) return "ERROR"; 
+    if (Number.isInteger(firstNumber) && Number.isInteger(lastNumber)){
+        let sum = 0;
+        while (firstNumber <= lastNumber) {
+            sum += firstNumber;
+            firstNumber++;
+        }
+	    return sum;
+    } else return "ERROR";
 };
 
 // Do not edit below this line
