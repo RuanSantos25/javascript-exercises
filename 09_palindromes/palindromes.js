5ed43022fe68246f3c9064bf3d03bb01df86a5c4
RuanSantos25 <[email protected]>
2024-08-01 19:27:46 -0300

    Add answers to 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let string = "";
+    let stringReverse = "";
+    const checkIsAlphanumeric = /^[a-zA-Z0-9]+$/;
+    for (let i = 0; i < words.length; i++) {
+        if (checkIsAlphanumeric.test(words[i])) {
+            string += words[i].toLowerCase();
+        }
+    }
+    stringReverse = string.split("").reverse().join("");
+    if (string === stringReverse) return true;
+    else return false;
 };
 
 // Do not edit below this line
