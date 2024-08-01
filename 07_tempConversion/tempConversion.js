d5e17a4de7c852002ebf9ed25041627e7c292288
RuanSantos25 <[email protected]>
2024-08-01 10:50:40 -0300

    Add answers to 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+	const tempCelsius = Number(((tempFahrenheit - 32) / (9 / 5)).toFixed(1));  // .toFixed() method returns a string, so we need to convert to number with the Number() function.
+	return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+	const tempFahrenheit = Number(((tempCelsius * 9 / 5 + 32)).toFixed(1));  // .toFixed() method returns a string, so we need to convert to number with the Number() function.
+	return tempFahrenheit;
 };
 
 // Do not edit below this line
